cfb742e7b9de4fdb9eaa636417c1b48996e92398
hter-petrosyan <hakobtp1987@gmail.com>
2025-11-11 12:58:04 +0400

    Generating the Skeleton Projects

---
+++ microservices/chapter-service/src/test/java/com/htp/microservices/core/chapter/ChapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htp.microservices.core.chapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
